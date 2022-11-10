5d50f0fdbdb59f57f6cf91785587ddaa9b762ef7
AlaaCool25 <97164012+AlaaCool25@users.noreply.github.com>
2022-11-10 20:51:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX = 0;
+noseY = 0;
+
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,350);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPose);
+}
+
+ function modelLoaded() {
+    console.log("PoseNet is initialized");
+ }
+
+function draw() {
+background('#E6E6FA');
+    document.getElementById("square_side").innerHTML = " Width and Height of Square is = " + difference + "px";
+    fill('#FAFA33');
+    stroke('#880808');
+    square(noseX, noseY, difference);
+}
+
+function gotPose(results) 
+{
+    if (results.length > 0) 
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX  + "noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor( leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+
+    }
+}
