11d6c7c8d40305c2692db2e8020015f6f2358149
AlaaCool25 <97164012+AlaaCool25@users.noreply.github.com>
2022-11-17 21:02:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 song = "";
 
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
 function preload() {
     song = loadSound("music.mp3");
 }
@@ -9,10 +15,31 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log("PoseNet is Initialized");
 }
 function draw() {
     image(video, 0, 0, 500, 500);
 }
 function play() {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+
+    }
 }
