1ab40b154d8ecd3fb350db510d97007fb0a13afd
AlaaCool25 <97164012+AlaaCool25@users.noreply.github.com>
2022-11-22 20:58:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 
+scoreLeftWrist = 0;
+
 function preload() {
     song = loadSound("music.mp3");
 }
@@ -24,6 +26,19 @@ function modelLoaded() {
 }
 function draw() {
     image(video, 0, 0, 500, 500);
+
+    fill("#ffe6f7");
+    stroke("#ddfffc");
+
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        InNumberLeftWristY = Number(leftWristY);
+        remove_decinals = floor(InNumberLeftWristY);
+        leftWristY_divide_1000 = remove_decinals/1000;
+        volume = leftWristY_divide_1000 *2;
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+    }
 }
 function play() {
     song.play();
@@ -33,6 +48,9 @@ function play() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
