149eb949ecce5848624e90d21380cb424ba17632
AlaaCool25 <97164012+AlaaCool25@users.noreply.github.com>
2022-11-24 20:28:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ rightWristX = 0;
 rightWristY = 0;
 
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 function preload() {
     song = loadSound("music.mp3");
@@ -30,6 +31,42 @@ function draw() {
     fill("#ffe6f7");
     stroke("#ddfffc");
 
+    if (scoreRightWrist > 0.2) {
+        
+        circle(rightWristX,rightWristY,20);
+
+        if (rightWristY > 0 && rightWristY <= 100) {
+            document.getElementById("speed").innerHTML = "Speed = 0.5x";
+            song.rate(0.5);
+    
+        }
+    
+        else if (rightWristY > 100 && rightWristY <= 200) {
+            document.getElementById("speed").innerHTML = "Speed = 1.0x";
+            song.rate(1);
+    
+        }
+    
+        else if (rightWristY > 200 && rightWristY <= 300) {
+            document.getElementById("speed").innerHTML = "Speed = 1.5x";
+            song.rate(1.5);
+    
+        }
+    
+        else if (rightWristY > 300 && rightWristY <= 400) {
+            document.getElementById("speed").innerHTML = "Speed = 2.0x";
+            song.rate(2);
+    
+        }
+    
+        else if (rightWristY > 400 && rightWristY <= 500) {
+            document.getElementById("speed").innerHTML = "Speed = 2.5x";
+            song.rate(2.5);
+    
+        }
+
+    }
+
     if (scoreLeftWrist > 0.2) {
         circle(leftWristX, leftWristY, 20);
         InNumberLeftWristY = Number(leftWristY);
@@ -49,7 +86,8 @@ function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
